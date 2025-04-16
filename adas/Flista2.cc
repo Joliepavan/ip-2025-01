34c115545e44f02ed5035764e9628ac9f8699219
Jolie Pavan <jolie@discente.ufg.br>
2025-04-16 09:59:53 -0300

    Create Flista2.cc

---
+++ adas/Flista2.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int X;
+    cin >> X; 
+
+    long long int fatorial= 1;
+    int N=1;
+
+    while (fatorial<X) {
+        N++;
+        fatorial*=N;
+    }
+
+    cout << N << "\n";
+    return 0;
+}
